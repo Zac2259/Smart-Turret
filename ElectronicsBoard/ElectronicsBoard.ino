15c4af913f74fe06500f06c2e636d5b2b08f7825
Zac2259 <0592323@schoolsnet.act.edu.au>
2022-05-31 11:55:33 +1000

    Update ElectronicsBoard.ino

--- ElectronicsBoard/ElectronicsBoard.ino
+++ ElectronicsBoard/ElectronicsBoard.ino
@@ -7,11 +7,75 @@
 RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
 
+// Traffic Lights - LED Outputs
+#define ledRed A0
+#define ledYellow A1 
+#define ledGreen A2
+
+// DC Motor & Motor Module - L298N
+#include <L298N.h>
+
+// Pin definition
+const unsigned int IN1 = 7;
+const unsigned int IN2 = 8;
+const unsigned int EN = 9;
+
+// Create one motor instance
+L298N motor(EN, IN1, IN2);
+
+// Servo
+#include <Servo.h>
+Servo myservo;
+
+//Potentiometer
+#define pot A3
+
+// Piezo Buzzer
+#define piezoPin 8
+
+// Sonar - HC-SR04
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin A4 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// Line Sensor
+#define lineSensorPin 3
+
+// Crash Sensor / Button
+#define crashSensor 4
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);           // Open serial communications and wait for port to open:
   while (!Serial) {
     delay(1);                   // wait for serial port to connect. Needed for native USB port only
+
+  // Traffic Lights - LED Outputs
+pinMode(ledRed, OUTPUT);
+pinMode(ledYellow, OUTPUT);
+pinMode(ledGreen, OUTPUT);
+
+// DC Motor & Motor Module - L298N
+motor.setSpeed(70);
+
+// Servo
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+//Potentiometer
+pinMode(pot, INPUT);
+
+// Piezo Buzzer
+pinMode(piezoPin,OUTPUT);
+
+// Sonar - HC-SR04
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+
+// Line Sensor
+pinMode(lineSensorPin, OUTPUT);
+
+// Crash Sensor / Button
+pinMode(crashSensor, INPUT);
+  
   }
 
   // SD Card initialisation
