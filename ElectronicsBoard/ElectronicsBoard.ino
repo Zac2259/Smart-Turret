121d0d4c74738fcd440cfe332b5be878fd9ca825
Zac2259 <0592323@schoolsnet.act.edu.au>
2022-05-31 11:21:58 +1000

    Update ElectronicsBoard.ino

--- ElectronicsBoard/ElectronicsBoard.ino
+++ ElectronicsBoard/ElectronicsBoard.ino
@@ -1,7 +1,79 @@
+// SD Card Module
+#include <SPI.h>
+#include <SD.h>
+
+// Real Time Clock (RTC)
+#include "RTClib.h"
+RTC_Millis rtc;     // Software Real Time Clock (RTC)
+DateTime rightNow;  // used to store the current time.
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);           // Open serial communications and wait for port to open:
+  while (!Serial) {
+    delay(1);                   // wait for serial port to connect. Needed for native USB port only
+  }
 
+  // SD Card initialisation
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  // Real Time Clock (RTC)
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+  Serial.println("initialization done.");
+  logEvent("System Initialisation...");
 }
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file on an SD card.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+
+  // Open the log file
+  File logFile = SD.open("events.csv", FILE_WRITE);
+  if (!logFile) {
+    Serial.print("Couldn't create log file");
+    abort();
+  }
+
+  // Log the event with the date, time and data
+  logFile.print(rightNow.year(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.month(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.day(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.hour(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.minute(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.second(), DEC);
+  logFile.print(",");
+  logFile.print(dataToLog);
+
+  // End the line with a return character.
+  logFile.println();
+  logFile.close();
+  Serial.print("Event Logged: ");
+  Serial.print(rightNow.year(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.month(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.day(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.hour(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.minute(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.second(), DEC);
+  Serial.print(",");
+  Serial.println(dataToLog);
+}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
