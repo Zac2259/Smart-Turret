702defbac2a790c7d8f8ade75325b6a58f9e8ee7
Zac2259 <0592323@schoolsnet.act.edu.au>
2022-06-14 11:43:48 +1000

    Update SmartTurret.ino

--- SmartTurret/SmartTurret.ino
+++ SmartTurret/SmartTurret.ino
@@ -156,9 +156,9 @@ void loop() {
 */
 
 void turretAdjustment() {
-//  int val = analogRead(pot);            // reads the value of the potentiometer (value between 0 and 1023)
-//  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-//  myservo.write(val);                  // sets the servo position according to the scaled value
+  int val = analogRead(pot);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
   // commented out to prevent servo from moving while programming
 }
 
